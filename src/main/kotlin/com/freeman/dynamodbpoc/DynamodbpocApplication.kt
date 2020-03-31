7c31a564aae67910f687ce3f1d358cc4e0343651
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2020-03-31 16:56:29 -0300

    Add project structure

---
+++ src/main/kotlin/com/freeman/dynamodbpoc/DynamodbpocApplication.kt
@@ -0,0 +1,11 @@
+package com.freeman.dynamodbpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DynamodbpocApplication
+
+fun main(args: Array<String>) {
+	runApplication<DynamodbpocApplication>(*args)
+}
---
+++ src/test/kotlin/com/freeman/dynamodbpoc/DynamodbpocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.freeman.dynamodbpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DynamodbpocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
